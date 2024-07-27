75b27bd8ea29c68d10fe4677f857f072a1b74a33
Isuru Madusanka Rodrigo <66762109+Madusanka-max@users.noreply.github.com>
2024-07-27 15:08:50 +0530

    Update app.service.ts

--- isuru/apps/api/src/app/app.service.ts
+++ isuru/apps/api/src/app/app.service.ts
@@ -5,4 +5,23 @@ export class AppService {
   getData(): { message: string } {
     return ({ message: 'Hello API' });
   }
+
+  //Start Asingment cal
+  SUM(number1:number, number2:number) {
+    return{ total: number1 + number2 };
+  }
+
+  SUB(number1:number, number2:number) {
+    return{ total: number1 - number2 };
+  }
+
+  MUL(number1:number, number2:number) {
+    return{ total: number1 * number2 };
+  }
+
+  DEV(number1:number, number2:number) {
+    return{ total: number1 / number2 };
+  }
+  //End Asingment cal
+  
 }
